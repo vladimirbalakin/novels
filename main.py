21a10e7daeb0fcc5c977623ee2fa1b18f27fcab0
vlligo <vladimir2balakin@yandex.ru>
2021-03-30 22:37:33 +0300

    Added reading from json file

--- main.py
+++ main.py
@@ -1,52 +1,16 @@
-words_with_capital_letters = ['I', 'And', 'Benefactor', 'But', 'Cafe', 'Bohemia', 'Close',
-                              'Crime', 'Da', 'Diamond', 'Dont', 'END', 'Edinburgh', 'Hatbox',
-                              'England', 'English', 'Europe', 'Father', 'French', 'Garden', 'Gardens', 'Available',
-                              'Day', 'He', 'Hello', 'His', 'Ill', 'Im', 'Is', 'Its', 'Lady', 'London', 'MajorGeneral',
-                              'Miss', 'Mr', 'My', 'Mysterious', 'Night', 'Now', 'Oh', 'Palace', 'Paris', 'Please',
-                              'Prince', 'Rescue', 'River', 'We', 'Were', 'What', 'Whats', 'When', 'Where', 'Who', 'Why',
-                              'You', 'Your', 'Youre', 'Shocking', 'Sir', 'Speak', 'Sundays', 'Surprise', 'THE', 'Thats',
-                              'This', 'Tonight',
-                              'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-                              'September', 'October', 'November', 'December', 'Monday', 'Tuesday', 'Wednesday',
-                              'Thursday', 'Friday', 'Saturday', 'Sunday', 'Afghanistan', 'Albania', 'Algeria',
-                              'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia',
-                              'Austria', 'Austrian Empire', 'Azerbaijan', 'Baden', 'Bahamas', 'Bahrain', 'Bangladesh',
-                              'Barbados', 'Bavaria', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bolivia',
-                              'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Brunswick and Lüneburg',
-                              'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon',
-                              'Canada', 'Cayman Islands', 'Central African Republic', 'Central American Federation',
-                              'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo Free State', 'The', 'Costa Rica',
-                              'Cote d’Ivoire', 'Croatia', 'Cuba', 'Cyprus', 'Czechia', 'Czechoslovakia',
-                              'Democratic Republic of the Congo', 'Denmark', 'Djibouti', 'Dominica',
-                              'Dominican Republic', 'Duchy of Parma', 'East Germany', 'German Democratic Republic',
-                              'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini',
-                              'Ethiopia', 'Federal Government of Germany', 'Fiji', 'Finland', 'France', 'Gabon',
-                              'Gambia', 'Georgia', 'Germany', 'Ghana', 'Grand Duchy of Tuscany', 'Greece', 'Grenada',
-                              'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Hanover',
-                              'Hanseatic Republics', 'Hawaii', 'Hesse', 'Holy See', 'Honduras', 'Hungary', 'Iceland',
-                              'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan',
-                              'Jordan', 'Kazakhstan', 'Kenya', 'Kingdom of Serbia', 'Yugoslavia', 'Kiribati', 'Korea',
-                              'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Lew Chew',
-                              'Loochoo', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar',
-                              'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania',
-                              'Mauritius', 'Mecklenburg-Schwerin', 'Mecklenburg-Strelitz', 'Mexico', 'Micronesia',
-                              'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Namibia',
-                              'Nassau', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria',
-                              'North German Confederation', 'North German Union', 'North Macedonia', 'Norway',
-                              'Oldenburg', 'Oman', 'Orange Free State', 'Pakistan', 'Palau', 'Panama', 'Papal States',
-                              'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Piedmont-Sardinia', 'Poland',
-                              'Portugal', 'Qatar', 'Republic of Genoa', 'Republic of Korea', 'South Korea',
-                              'Republic of the Congo', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis',
-                              'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino',
-                              'Sao Tome and Principe', 'Saudi Arabia', 'Schaumburg-Lippe', 'Senegal', 'Serbia',
-                              'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands',
-                              'Somalia', 'South Africa', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname',
-                              'Sweden', 'Switzerland', 'Syria', 'Tajikistan', 'Tanzania', 'Texas', 'Thailand',
-                              'Timor-Leste', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey',
-                              'Turkmenistan', 'Tuvalu', 'Two Sicilies', 'Uganda', 'Ukraine',
-                              'Union of Soviet Socialist Republics', 'United Arab Emirates', 'United Kingdom',
-                              'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'Württemberg', 'Yemen',
-                              'Zambia', 'Zimbabwe']
+import json
+
+
+def read_from_json_file(name):
+    with open(name, "r") as input_file:
+        data = json.load(input_file)
+    input_file.close()
+    return data
+
+
+def write_to_json_file(name, data):
+    with open(name, "w") as output_file:
+        json.dump(data, output_file)
 
 
 def reformat_file(name_of_the_file: str) -> None:
@@ -99,6 +63,7 @@ def get_names_of_heroes(sentences: list) -> list:
     return list(heroes)
 
 
+words_with_capital_letters = read_from_json_file("words_with_capital_letters.json")
 main_file = "The Rajah's Diamond.txt"
 reformat_file(main_file)
 text = extract_text_from_file(main_file)
