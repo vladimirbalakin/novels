8f2520ac77f9061689b0a74d8a1925d403685038
vlligo <vladimir2balakin@yandex.ru>
2021-04-13 22:33:48 +0300

    Added building a graph

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import json
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.drawing.nx_pydot import graphviz_layout
+import pydot
 
 
 def read_from_json_file(name):
@@ -60,7 +66,27 @@ def get_names_of_heroes(sentences: list) -> list:
                     pass
         except:
             pass
+    for i in list(heroes):
+        if i[:-1] in heroes:
+            heroes.remove(i)
     return list(heroes)
+
+
+def get_graph_from_sentences(sentences: list, heroes: list):
+    g = nx.Graph()
+    g.add_nodes_from(heroes)
+    for i in sentences:
+        list_of_nodes = []
+        for word in i.split():
+            if word in heroes or (word[-1] == 's' and word[:-1] in heroes):
+                if word[-1] == 's' and word[:-1] in heroes:
+                    list_of_nodes.append(word[:-1])
+                else:
+                    list_of_nodes.append(word)
+        for j in range(len(list_of_nodes)):
+            for k in range(j + 1, len(list_of_nodes)):
+                g.add_edge(list_of_nodes[k], list_of_nodes[j])
+    return g
 
 
 words_with_capital_letters = read_from_json_file("words_with_capital_letters.json")
@@ -69,4 +95,15 @@ reformat_file(main_file)
 text = extract_text_from_file(main_file)
 sentences = get_sentences_from_text(text)
 names = sorted(get_names_of_heroes(sentences))
+g = get_graph_from_sentences(sentences, names)
+pos = graphviz_layout(g, prog="dot")
+labels = defaultdict(str)
+for node in g.nodes():
+    labels[node] = node
+
+# nx.draw_networkx_nodes(g, pos, names)
+nx.draw_networkx_labels(g, pos, labels)
+# nx.draw_networkx_edges(g, pos, edgelist=g.edges)
+nx.draw(g, pos)
+plt.show()
 print(names)
